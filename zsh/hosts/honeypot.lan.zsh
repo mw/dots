1167959b6ff06fa66b2d1e6d7debb14ffba2ce80
Marc Winners <marcwinners@gmail.com>
2020-12-03 19:10:20 -0800

    Update

---
+++ zsh/hosts/honeypot.lan.zsh
@@ -0,0 +1,5 @@
+export GOROOT=/usr/local/opt/go/libexec
+export GOPATH=/Users/marc/.go/
+export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.7/site-packages
+export PATH=/usr/local/go/bin:$PATH:$GOPATH/bin:/usr/local/sbin
